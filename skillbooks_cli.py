db159cbf7403eba9b11d0623053e301ce3e3a2e4
Isabelle Hurbain-Palatin <isabelle@palatin.fr>
2014-10-30 10:43:54 +0100

    removed prettyprint module

--- skillbooks_cli.py
+++ skillbooks_cli.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python
 import evelink
-import pprint
 import sqlite3
 import pprint
 
 from keys import keyid, vcode
 from config import stationid, smallcontainerid, dbname, booklist
 from skillbooks_stock import get_quantities
-pp = pprint.PrettyPrinter(indent=2)
 
 quantities = get_quantities()
 
