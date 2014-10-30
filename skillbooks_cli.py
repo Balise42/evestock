ef8b262c3ccfe4db460cf136e94aae48e48f8a0c
Isabelle Hurbain-Palatin <isabelle@palatin.fr>
2014-10-30 22:44:33 +0100

    now also gets the other items of the hangar if wanted

--- skillbooks_cli.py
+++ skillbooks_cli.py
@@ -7,7 +7,7 @@ from keys import keyid, vcode
 from config import stationid, smallcontainerid, dbname, booklist
 from skillbooks_stock import get_quantities
 
-quantities = get_quantities()
+quantities = get_quantities(True)
 
 for quantity in quantities:
     print quantity["name"], quantity["quantity"]
