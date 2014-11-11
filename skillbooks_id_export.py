60f878376718c691573b0c19e27687b9deca4a9a
Isabelle Hurbain-Palatin <isabelle@palatin.fr>
2014-11-11 18:02:23 +0100

    large refactor, may or may not work, not tested!

--- skillbooks_id_export.py
+++ skillbooks_id_export.py
@@ -3,15 +3,26 @@
 import sqlite3
 from config import dbname, bookids
 
-conn = sqlite3.connect(dbname)
-conn.text_factory = str
-c = conn.cursor()
-items = {}
-c.execute("SELECT typeId, typeName from invTypes")
-f = open(bookids, 'w')
-for(itemid, name) in c:
-        f.write(itemid.__str__())
-        f.write(' ')
-        f.write(name)
-        f.write('\n')
+class IdExport:
+  def __init__(self):
+    self.file_to_write = open(bookids, 'w')
+    self.conn = sqlite3.connect(dbname)
+    self.conn.text_factory = str
+    self.cursor = self.conn.cursor()
 
+  def get_items(self):
+    self.cursor.execute("SELECT typeId, typeName from invTypes")
+
+  def run(self):
+    self.get_items()
+    for(itemid, itemname) in self.cursor:
+      self.write_item_to_file(itemid, itemname)
+
+  def write_item_to_file(self, itemid, itemname):
+      file_to_write.write(itemid.__str__())
+      file_to_write.write(' ')
+      file_to_write.write(itemname)
+      file_to_write.write('\n')
+
+if __name__ == '__main__':
+  IdExport().run()
