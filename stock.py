9ba3c8102607e1ff5f99b14f6574b5d1f7c05fcf
Isabelle Hurbain-Palatin <isabelle@palatin.fr>
2014-12-01 22:52:04 +0100

    refactoring in progress, may or may not compile, and i need my bed

--- stock.py
+++ stock.py
@@ -10,18 +10,28 @@ from config import containername, dbname, booklist, bookids, allitems, stationna
 from container import Container
 from station import Station
 
+# Gets the stock of the configured container in the configured station
+# and compares it to the list of items that we want to track
 class Stock:
   def __init__(self):
     self.station = Station(stationname)
     self.container = Container(containername, self.station)
+    self.compute_stock()
+
+  def compute_stock(self):
     self.quantities = self.container.quantities
     self.get_item_ids_from_db_dump()
-    self.compute_list_of_contents()
+    self.compute_stock_from_list()
 
-  def compute_list_of_contents(self):
+  def compute_stock_from_list(self):
+    self.get_items_from_list_quantities()
+    if(allitems):
+      self.get_items_not_in_list_quantities()
+
+  def get_item_from_list_quantities(self):
     booknames = [s.strip() for s in open(booklist)]
 
-    bookquantities = []
+    self.itemquantities = []
     for name in booknames:
         quant = {}
         quant["name"] = name
@@ -37,17 +47,17 @@ class Stock:
         else:
             quant["quantity"] = 0
             quant["class"] = 'text-danger'
-        bookquantities.append(quant)
+        self.itemquantities.append(quant)
 
+  def get_items_not_in_list_quantities(self):
     if(allitems):
-        for skillbook in self.container.contents:
-            if not self.itemsbyid[skillbook["item_type_id"]] in booknames:
+        for item in self.container.contents:
+            if not self.itemsbyid[item["item_type_id"]] in booknames:
                 quant = {}
-                quant["name"] = self.itemsbyid[skillbook["item_type_id"]]
+                quant["name"] = self.itemsbyid[item["item_type_id"]]
                 quant["class"] = 'text-warning'
-                quant["quantity"] = self.quantities[skillbook["item_type_id"]]
-                bookquantities.append(quant)
-    self.list_of_items = bookquantities
+                quant["quantity"] = self.quantities[item["item_type_id"]]
+                self.itemquantities.append(quant)
 
   def get_item_ids_from_db_dump(self):
     lines = [s.strip() for s in open(bookids)]
