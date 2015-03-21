f0457a05308c2d8e41de282cbde62d0c62322ed1
Isabelle Hurbain-Palatin <isabelle@palatin.fr>
2015-03-21 19:49:11 +0100

    added basicerror message for raised exceptions

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ dbname = "eve-dump.db" #optional, only if you want to regenerate the item ids li
 booklist = "skillbooks-list" # list of the skillbooks to track
 bookids = "skillbooks-ids" # id <-> item name list (from eve static dump)
 allitems = True #True to return all the items of the container, False to return only those from the list
+error_action = "find someone to fix it" # what action message to display in case errors persist
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import jinja2
 import os
 
 from stock import Stock
+from config import error_action
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)), 
@@ -14,6 +15,10 @@ class MainHandler(webapp2.RequestHandler):
       bookquantities = sorted(stock.list_of_items, key=lambda quantity: quantity['class']+quantity['name'])
       template = JINJA_ENVIRONMENT.get_template('index.html')
       self.response.write(template.render({'bookquantities': bookquantities}))
+
+    def handle_exception(self, exception, debug_mode):
+      template = JINJA_ENVIRONMENT.get_template('error.html')
+      self.response.write(template.render({'action' : error_action}))
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
